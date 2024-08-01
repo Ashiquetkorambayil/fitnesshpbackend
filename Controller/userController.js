4e84448d9f0a848a2feb78d469b642841c88adcc
Ashiquetkorambayil <102928710+Ashiquetkorambayil@users.noreply.github.com>
2024-08-01 18:40:26 +0530

    modified the user controller

--- Controller/userController.js
+++ Controller/userController.js
@@ -511,8 +511,8 @@ exports.forgotPassword = asyncHandler(async (req, res) => {
         const transporter = nodemailer.createTransport({
             service: 'Gmail',
             auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS
+                user: 'fitnesshp487@gmail.com',
+                pass: 'kbry cmru lyrx mzza'
             }
         });
 
