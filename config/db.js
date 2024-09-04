0efa0bf83c045b2b257c55fa37e0a01c9d30bad5
Ashiquetkorambayil <102928710+Ashiquetkorambayil@users.noreply.github.com>
2024-09-04 13:01:33 +0530

    changed the db

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://tecnaviswebsolutions:XcsP1AQoaczFkL0R@cluster0.yxamk2w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
-        // await mongoose.connect('mongodb+srv://ashiquetk860628:JxYXct9qJ7F3xszE@gymadmin.gmvkych.mongodb.net/?retryWrites=true&w=majority&appName=gymadmin');
+        // await mongoose.connect('mongodb+srv://tecnaviswebsolutions:XcsP1AQoaczFkL0R@cluster0.yxamk2w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect('mongodb+srv://ashiquetk860628:JxYXct9qJ7F3xszE@gymadmin.gmvkych.mongodb.net/?retryWrites=true&w=majority&appName=gymadmin');
         console.log('MongoDB Connected');
     } catch (error) {
         console.error(error.message);
